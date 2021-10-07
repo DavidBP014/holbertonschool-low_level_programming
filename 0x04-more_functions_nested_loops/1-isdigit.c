0f081d50c1dfb20d69ab3494a9676ab3e513f278
DavidBP014 <juanbp014@gmail.com>
2021-10-07 17:51:52 -0500

    new change

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** 
+ * 
+ * include _isdigit 
+ * 
+ * Returns 1 if c is a digit
+ * Returns 0 otherwise
+ * 
+ * Write a function that checks for a digit (0 through 9)
+ */
+
+int _isupper(int c)
+{
+
+if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+
+
+}
+ 
