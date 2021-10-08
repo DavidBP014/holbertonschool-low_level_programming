30c1b49e0188f34c44ca46c8b4f8dcdb59e41bd6
DavidBP014 <juanbp014@gmail.com>
2021-10-08 16:05:33 -0500

    oinoub

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,43 +1,37 @@
-#include <stdio.h>
-<<<<<<< HEAD
+#include "main.h"
 
 /**
- * main - prints largest prime factor.
- * Return: Always 0.
- */
-
-int main(void)
-{
-	long int n, fp;
-
-	n = 612852475143;
-	for (fp = 2; fp <= n; fp++)
-	{
-		if (n % fp == 0)
-		{
-			n /= fp;
-			fp--;
-		}
-	}
-	printf("%ld\n", fp);
-	return (0);
-=======
-/**
- * main - factor
+ * main - prints the largest prime factor of the number
  *
  * Return: 0
  */
+
 int main(void)
 {
-  unsigned long int a, i;
+	long int num1 = 612852475143;
+	long int div = 2;
+	/*long int ans = 0;*/
+	long int maxfact = 0;
 
-  a = 612852475143;
-  for (i = 3; i < (a / 2); i = i + 2)
-    {
-      while ((a % i == 0) && (a != i))
-	a = a / i;
-    }
-  printf("%lu\n", a);
-  return (0);
->>>>>>> 994d77e2c70efa6c385ef8ee6208ceec86557145
+	while (num1 != 0)
+	{
+
+		if (num1 % div != 0)
+		{
+			div = div + 1;
+		}
+		else
+		{
+			maxfact = num1;
+			num1 = num1 / div;
+			if (num1 == 1)
+			{
+				printf("%ld", maxfact);
+				break;
+			}
+		}
+	}
+
+	putchar(10);
+	return (0);
 }
