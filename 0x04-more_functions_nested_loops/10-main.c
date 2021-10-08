fa6edba9730e11c20606d7c26ffe45264d9004ba
DavidBP014 <juanbp014@gmail.com>
2021-10-08 16:01:32 -0500

    bkub

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-print_triangle(3);
-print_triangle(15);
-print_triangle(2);
-print_triangle(1);
-return (0);
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    return (0);
 }
